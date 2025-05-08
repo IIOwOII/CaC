12daa332fcad13b2f25ccabca12df0d90e685178
IIOwOII <sasd9750o@naver.com>
2025-05-08 10:26:39 +0900

    ahhhhhh

--- MCmod/src/main/java/net/owo/cac/procedures/AiCatProcedure.java
+++ MCmod/src/main/java/net/owo/cac/procedures/AiCatProcedure.java
@@ -10,15 +10,12 @@ public class AiCatProcedure {
 	public static void execute(LevelAccessor world, Entity entity) {
 		if (entity == null)
 			return;
-		double distance_player = 0;
 		if (CacModVariables.MapVariables.get(world).Switch_AI) {
-			if (entity.getPersistentData().getDouble("C_Timer") <= 0) {
-				entity.getPersistentData().putDouble("C_Timer", 0.5);
+			if (CacModVariables.MapVariables.get(world).Time_AI == 0 && !world.isClientSide()) {
 				if (entity instanceof Mob _entity)
 					_entity.getNavigation().moveTo((CacModVariables.MapVariables.get(world).Pos_player.x()), (CacModVariables.MapVariables.get(world).Pos_player.y()), (CacModVariables.MapVariables.get(world).Pos_player.z()),
 							(0.565685424949238 * Math.pow(CacModVariables.MapVariables.get(world).Dat_difficulty_absolute, 0.5)));
 			}
-			entity.getPersistentData().putDouble("C_Timer", (entity.getPersistentData().getDouble("C_Timer") - 0.05));
 		}
 	}
 }
