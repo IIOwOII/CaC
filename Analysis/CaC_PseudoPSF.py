dfad7512527fdd89fab848c90d6ec02abb3c23eb
IIOwOII <sasd9750o@naver.com>
2025-06-20 15:37:43 +0900

    sleep

--- Analysis/CaC_PseudoPSF.py
+++ Analysis/CaC_PseudoPSF.py
@@ -6,25 +6,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #%%
-subj = 'jmj03'
+subjs = ['pbj02','pbj03','pbj04']
 task = 'test_chased'
 
 dir_main = f'{os.getcwd()}/../MCmod/run/cacutil/behaviors'
-dir_beh = f'{dir_main}/{subj}/{task}'
-with open(f'{dir_beh}/log_event.json') as f:
-    log_event = json.load(f)['cac']
-with open(f'{dir_beh}/log_gameplay.json') as f:
-    log_gameplay = json.load(f)['cac']
-with open(f'{dir_beh}/log_position.json') as f:
-    log_position = json.load(f)['cac']
+dir_beh = [f'{dir_main}/{subj}/{task}' for subj in subjs]
 
+log_event = []
+log_gameplay = []
+log_position = []
+for beh in dir_beh:
+    with open(f'{beh}/log_event.json') as f:
+        log_event.append(json.load(f)['cac'])
+    with open(f'{beh}/log_gameplay.json') as f:
+        log_gameplay.append(json.load(f)['cac'])
+    with open(f'{beh}/log_position.json') as f:
+        log_position.append(json.load(f)['cac'])
 
-#%%
 T = 600
-trials = len(log_gameplay['time'])
-
-is_chasing = np.array(log_gameplay['type'])==0
-is_chased = np.array(log_gameplay['type'])==1
 
 
 #%%
@@ -104,45 +103,57 @@ def get_r(trial):
 
 #%% 3D r(t)
 
-fig = plt.figure(figsize=(8,4), dpi=300)
-ax1 = fig.add_subplot(1,2,1, projection='3d')
-ax2 = fig.add_subplot(1,2,2, projection='3d')
+# fig = plt.figure(figsize=(8,4), dpi=300)
+# ax1 = fig.add_subplot(1,2,1, projection='3d')
+# ax2 = fig.add_subplot(1,2,2, projection='3d')
 
-font = {"fontsize":"medium", "color":"#448822", "fontweight":"bold"}
+# font = {"fontsize":"medium", "color":"#448822", "fontweight":"bold"}
 
-ax1.axhline(1, color='purple', linestyle='-.', linewidth=1)
-ax2.axhline(1, color='purple', linestyle='-.', linewidth=1)
+# ax1.axhline(1, color='purple', linestyle='-.', linewidth=1)
+# ax2.axhline(1, color='purple', linestyle='-.', linewidth=1)
 
-for i in range(trials):
-    r, t = get_r(i)
-    c = hex(int(32 + i*(223/trials)))[-2:]
-    if (log_gameplay['type'][i] == 0):
-        ax1.plot(t/20, log_gameplay['difficulty_absolute'][i], r, 
-                 color=f'#{c}0000', linewidth=0.7, alpha=0.8)
-    else:
-        ax2.plot(t/20, log_gameplay['difficulty_absolute'][i], r, 
-                 color=f'#0000{c}', linewidth=0.7, alpha=0.8)
+# for i in range(trials):
+#     r, t = get_r(i)
+#     c = hex(int(32 + i*(223/trials)))[-2:]
+#     if (log_gameplay['type'][i] == 0):
+#         ax1.plot(t/20, log_gameplay['difficulty_absolute'][i], r, 
+#                  color=f'#{c}0000', linewidth=0.7, alpha=0.8)
+#     else:
+#         ax2.plot(t/20, log_gameplay['difficulty_absolute'][i], r, 
+#                  color=f'#0000{c}', linewidth=0.7, alpha=0.8)
 
-ax1.set_title('Distance between player and opponent (within trial)')
+# ax1.set_title('Distance between player and opponent (within trial)')
 
-ax1.set_xlabel('time(sec)', fontdict=font, labelpad=4)
-ax2.set_xlabel('time(sec)', fontdict=font, labelpad=4)
+# ax1.set_xlabel('time(sec)', fontdict=font, labelpad=4)
+# ax2.set_xlabel('time(sec)', fontdict=font, labelpad=4)
 
-ax1.set_xlim((-5,T/20))
-ax1.set_ylim((0.8,1.2))
-ax1.set_yticks(np.arange(0.8,1.2,0.1))
-ax1.set_zlim((0,18))
-ax1.set_zticks(np.arange(0,17,4))
+# ax1.set_xlim((-5,T/20))
+# ax1.set_ylim((0.8,1.2))
+# ax1.set_yticks(np.arange(0.8,1.2,0.1))
+# ax1.set_zlim((0,18))
+# ax1.set_zticks(np.arange(0,17,4))
 
-ax2.set_xlim((-5,T/20))
-ax2.set_ylim((0.8,1.2))
-ax2.set_yticks(np.arange(0.8,1.2,0.1))
-ax2.set_zlim((0,18))
-ax2.set_zlabel('distance(block)', fontdict=font, labelpad=4)
-ax2.set_zticks(np.arange(0,17,4))
+# ax2.set_xlim((-5,T/20))
+# ax2.set_ylim((0.8,1.2))
+# ax2.set_yticks(np.arange(0.8,1.2,0.1))
+# ax2.set_zlim((0,18))
+# ax2.set_zlabel('distance(block)', fontdict=font, labelpad=4)
+# ax2.set_zticks(np.arange(0,17,4))
+
+
+#%% t-rho graph
+
+fig, ax = plt.subplots(figsize=(6,8), dpi=300)
+
+log_gameplay[]
+
+ax.plot()
+
+ax.set_xlim((0.79, 1.21))
+ax.set_ylim((T/20))
 
 
 #%%
 
-plt.tight_layout()
+fig.tight_layout()
 plt.show()
