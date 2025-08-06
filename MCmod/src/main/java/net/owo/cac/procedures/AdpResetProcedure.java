ed19b23f8f1683a66672dffb143e8ec3be0d3f92
IIOwOII <sasd9750o@naver.com>
2025-08-06 14:56:50 +0900

    kkk

--- MCmod/src/main/java/net/owo/cac/procedures/AdpResetProcedure.java
+++ MCmod/src/main/java/net/owo/cac/procedures/AdpResetProcedure.java
@@ -4,7 +4,14 @@ import net.minecraft.world.scores.criteria.ObjectiveCriteria;
 import net.minecraft.world.scores.Scoreboard;
 import net.minecraft.world.scores.Objective;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.chat.Component;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.CommandSource;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.advancements.Advancement;
 
 public class AdpResetProcedure {
 	public static void execute(Entity entity) {
@@ -17,6 +24,21 @@ public class AdpResetProcedure {
 			if (_so == null)
 				_so = _sc.addObjective("adv_moving", ObjectiveCriteria.DUMMY, Component.literal("adv_moving"), ObjectiveCriteria.RenderType.INTEGER);
 			_sc.getOrCreatePlayerScore(_ent.getScoreboardName(), _so).setScore(0);
+		}
+		{
+			Entity _ent = entity;
+			if (!_ent.level().isClientSide() && _ent.getServer() != null) {
+				_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level() instanceof ServerLevel ? (ServerLevel) _ent.level() : null, 4,
+						_ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent), "advancement revoke @p from cac:adv_cac");
+			}
+		}
+		if (entity instanceof ServerPlayer _player) {
+			Advancement _adv = _player.server.getAdvancements().getAdvancement(new ResourceLocation("cac:adv_cac"));
+			AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
+			if (!_ap.isDone()) {
+				for (String criteria : _ap.getRemainingCriteria())
+					_player.getAdvancements().award(_adv, criteria);
+			}
 		}
 	}
 }
