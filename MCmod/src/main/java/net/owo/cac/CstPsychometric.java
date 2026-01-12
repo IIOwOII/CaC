e1ecf7c74677a3b067a4d33d749b0ac83bd499ae
IIOwOII <sasd9750o@naver.com>
2026-01-12 18:23:00 +0900

    hi

--- MCmod/src/main/java/net/owo/cac/CstPsychometric.java
+++ MCmod/src/main/java/net/owo/cac/CstPsychometric.java
@@ -20,6 +20,57 @@ public class CstPsychometric {
 		return L;
 	}
 
+	public static double[] calLikelihood(double[] L, double[] P, boolean iswin) {
+		// L is log likelihood of parameter
+		// P is win probability given difficulty
+		double[] L_next = new double[L.length];
+		double p = 0;
+		double Z_log = Math.log(calZ(L, P, iswin));
+		
+		for (int k=0; k<L.length; k++) {
+			if (iswin) {
+				p = P[k];
+			} else {
+				p = 1 - P[k];
+			}
+			L_next[k] = L[k] + Math.log(p) - Z_log;
+		}
+
+		return L_next;
+	}
+
+	public static double calEntropy(double[] L) {
+		// L is log likelihood of parameter (weight)
+		double H = 0;
+		for (int k=0; k<L.length; k++) {
+			H -= (L[k] * Math.exp(L[k]));
+		}
+		return H;
+	}
+
+	public static double[] calInfoGain(double H_curr, double[] H_win, double[] H_lose, double[] PI) {
+		double[] EIG = new double[H_win.length];
+		for (int x=0; x<H_win.length; x++){
+			EIG[x] = H_curr - PI[x]*H_win - (1-PI[x])*H_lose;
+		}
+		return EIG;
+	}
+
+	public static double calZ(double[] L, double[] P, boolean iswin) {
+		// Z is sum of P(x)*P(theta)
+		double Z = 0;
+		double p = 0;
+		for (int k=0; k<L.length; k++) {
+			if (iswin) {
+				p = P[k];
+			} else {
+				p = 1-P[k];
+			}
+			Z += (p * Math.exp(L[k]));
+		}
+		return Z;
+	}
+
 	public static int flattenIndex(double sa, double sb, double sc, double sd) {
 		int SA = (int)sa;
 		int SB = (int)sb;
@@ -55,5 +106,14 @@ public class CstPsychometric {
 		}
 		return Math.log(Y_sum);
 	}
-	
+
+	public static int argmax(double[] arr) {
+		int i_max = 0;
+		for (int i=1; i<arr.length; i++) {
+			if (arr[i_max] < arr[i]) {
+				i_max = i;
+			}
+		}
+		return i_max;
+	}
 }
