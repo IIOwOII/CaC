8e40374da9dcf8f55e0887278679711c49605c26
IIOwOII <sasd9750o@naver.com>
2026-01-21 19:01:45 +0900

    fighting

--- Analysis/CaC_Sim.py
+++ Analysis/CaC_Sim.py
@@ -3,11 +3,13 @@ import numpy as np
 import json
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
-from sklearn.metrics import r2_score
 
 #%%
 def func_logistic(X, a, b, c, d):
     return c+((1-d-c)/(1+np.exp((X-a)/b)))
+
+def func_exponential(X, a):
+    return np.exp(X-a)
 
 
 #%%
@@ -30,7 +32,7 @@ c2_wl = np.array(chased['winlose'])
 
 
 #%%
-def plot_rho_t(rho, t, spawn, spawn_idx=0):
+def plot_rho_t(rho, t, spawn, spawn_idx=0, task='chasing', func='logistic'):
     c_x = np.arange(0.8, 1.2, 0.01)
     if (spawn_idx > 0):
         rho = rho[spawn==(spawn_idx-1)]
@@ -38,12 +40,14 @@ def plot_rho_t(rho, t, spawn, spawn_idx=0):
     elif (spawn_idx < 0):
         rho = rho[spawn!=(-spawn_idx-1)]
         t = t[spawn!=(-spawn_idx-1)]
-    t_norm = t/50
+        
+    T = max(t)
+    t_norm = t/T
     
     fig, ax = plt.subplots(figsize=(4,3), dpi=300)
     
     ax.set_xlim([0.78,1.2])
-    ax.set_ylim([-2,52])
+    ax.set_ylim([-0.02*T,1.02*T])
     for side in ['right', 'top', 'bottom']:
         ax.spines[side].set_visible(False)
     
@@ -56,9 +60,17 @@ def plot_rho_t(rho, t, spawn, spawn_idx=0):
     
     ax.scatter(rho, t, s=1, color='k', alpha=0.5, zorder=1)
     
-    popt, pcov = curve_fit(func_logistic, rho, t_norm, p0=[1,0,0,0], 
-                           bounds=([0.8,-100,0,0],[1.2,100,0.2,0.2]), maxfev=20000)
-    ax.plot(c_x, 50*func_logistic(c_x, *popt), color='forestgreen', zorder=2)
+    if (func=='logistic'):
+        if (task=='chasing'):
+            beta_sign = -1
+        elif (task=='chased'):
+            beta_sign = 1
+        beta_b0 = beta_sign*0.005
+        beta_b1 = beta_sign*0.1
+        
+        popt, pcov = curve_fit(func_logistic, rho, t_norm, p0=[1,beta_sign*0.04,0,0], 
+                               bounds=([0.9,min(beta_b0,beta_b1),0,0],[1.1,max(beta_b0,beta_b1),0.1,0.1]), maxfev=20000)
+        ax.plot(c_x, T*func_logistic(c_x, *popt), color='forestgreen', zorder=2)
     
     return popt, pcov
 
@@ -89,13 +101,44 @@ def plot_rho_p(rho, p, spawn, spawn_idx=0):
     
     return popt, pcov
 
+
+def plot_rho_mu(rho, t):
+    c_x = np.arange(0.8, 1.2, 0.01)
+    c_y = np.zeros(c_x.size)
+    for i, x in enumerate(c_x):
+        x = np.around(x, 2)
+        c_y[i] = np.mean(t[rho==x])
+    print(c_y)
+    T = max(t)
+    
+    fig, ax = plt.subplots(figsize=(4,3), dpi=300)
+    
+    ax.set_xlim([0.78,1.2])
+    ax.set_ylim([-0.02*T,1.02*T])
+    for side in ['right', 'top', 'bottom']:
+        ax.spines[side].set_visible(False)
+    
+    ax.axhline(0, linewidth=0.8, linestyle='-', color='k', zorder=-1)
+    ax.axhline(10, linewidth=0.4, linestyle='-', color='gray', alpha=0.2, zorder=-1)
+    ax.axhline(20, linewidth=0.4, linestyle='-', color='gray', alpha=0.2, zorder=-1)
+    ax.axhline(30, linewidth=0.8, linestyle='-.', color='orangered', zorder=-1)
+    ax.axhline(40, linewidth=0.4, linestyle='-', color='gray', alpha=0.2, zorder=-1)
+    ax.axhline(50, linewidth=0.4, linestyle='-', color='gray', alpha=0.2, zorder=-1)
+    
+    ax.scatter(rho, t, s=1, color='k', alpha=0.5, zorder=1)
+    ax.plot(c_x, c_y, color='forestgreen', zorder=2)
+    
+
 #%% plot
 
-# rt1_popt, rt1_pcov = plot_rho_t(c1_diff, c1_time, c1_spawn)
-# rt2_popt, rt2_pcov = plot_rho_t(c2_diff, c2_time, c2_spawn)
+# rt1_popt, rt1_pcov = plot_rho_t(c1_diff, c1_time, c1_spawn, task='chasing')
+# rt2_popt, rt2_pcov = plot_rho_t(c2_diff, c2_time, c2_spawn, task='chased')
 
-rp1_popt, rp1_pcov = plot_rho_p(c1_diff, c1_wl, c1_spawn)
-rp2_popt, rp2_pcov = plot_rho_p(c2_diff, c2_wl, c2_spawn)
+# rp1_popt, rp1_pcov = plot_rho_p(c1_diff, c1_wl, c1_spawn)
+# rp2_popt, rp2_pcov = plot_rho_p(c2_diff, c2_wl, c2_spawn)
+
+plot_rho_mu(c1_diff, c1_time)
+plot_rho_mu(c2_diff, c2_time)
 
 #indifference y=0.5인 위치 세로선과 coeff 주기
 plt.show()
